353ecc8b6366d66e52d4e0167337eacc0185c8af
Robin Mattheussen <robin.mattheussen@gmail.com>
2015-08-23 09:01:37 +0200

    Updated Tweet model

--- app/twitter/Tweet.scala
+++ app/twitter/Tweet.scala
@@ -1,13 +1,50 @@
 package twitter
 
-import play.api.libs.json.JsValue
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import scala.util.{Try, Failure, Success}
 
-/** Container for the JSON tweet message received from the stream **/
-// FIXME: Provide actual deserialized model for the message
-case class Tweet(json: String)
+/** Container for the tweet message received from the stream **/
+// FIXME: Add potential Geolocation
+case class Tweet(
+	id: String,						// "id_str"
+	createdAt: String,				// "created_at"
+	createdAtMs: String,			// "timestamp_ms"
+	text: String,					// "text"
+	userName: String,				// "user": { ... , ... , "name": "Joe", ... }
+	screenName: String				// "user": { ... , ... , "screen_name": "therealjoe", ... }
+)
+{
+	implicit val tweetWrites = new Writes[Tweet] {
+		def writes(tweet: Tweet) = Json.obj(
+			"id" 			-> tweet.id,
+			"created_at" 	-> tweet.createdAt,
+			"created_at_ms" -> tweet.createdAtMs,
+			"text" 			-> tweet.text,
+			"userName" 		-> tweet.userName,
+			"screenName" 	-> tweet.screenName
+		)
+	}
+
+	def toJson: JsValue = {
+		Json.toJson(this)
+	}
+}
 
 object Tweet {
-	def apply(json: JsValue): Tweet = {
-		Tweet(json \ "text" toString)
+	implicit val tweetReads: Reads[Tweet] = (
+		(JsPath \ "id").read[String] and
+		(JsPath \ "created_at").read[String] and
+		(JsPath \ "timestamp_ms").read[String] and
+		(JsPath \ "text").read[String] and
+		(JsPath \ "user" \ "name").read[String] and
+		(JsPath \ "user" \ "screen_name").read[String]
+	)(Tweet.apply _ )
+
+	def fromJson(json: JsValue): Try[Tweet] = {
+		json.validate[Tweet] match {
+			case s: JsSuccess[Tweet] => Success(s.get)
+			case e: JsError => Failure(new Exception("Unable to validate JsValue as JSON Tweet"))
+		}
 	}
 }
