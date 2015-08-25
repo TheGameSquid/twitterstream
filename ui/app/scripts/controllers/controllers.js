8f04bb150161074a03d8f5dcfbe918079b081e47
Robin Mattheussen <robin.mattheussen@gmail.com>
2015-08-25 02:06:29 +0200

    Updated css elements + reshuffled controller structure

--- ui/app/scripts/controllers/controllers.js
+++ ui/app/scripts/controllers/controllers.js
@@ -1,18 +1,22 @@
 /// <reference path="../../../typings/angularjs/angular.d.ts" />
 
-'user strict';
+'use strict';
 
 angular.module('twitterstream.controllers', ['twitterstream.services'])
 	.controller('tweetController', ['$scope', 'tweetService', function($scope, tweetService) {
 		$scope.tweets = [];
 		
-		tweetService.getTweets(function(response) {
-        	var tweet = JSON.parse(response.data);
+		$scope.addTweet = function(tweet) {
 			if ($scope.tweets.length == 100) {
 				$scope.tweets.pop();
 			}
-			tweet.created_at_unix = tweet.created_at_ms.slice(0, -3)
+			tweet.created_at_unix = tweet.created_at_ms.slice(0, -3);
           	$scope.tweets.unshift(tweet);
 			$scope.$apply();
+		};
+		
+		tweetService.getTweets(function(response) {
+        	var tweet = JSON.parse(response.data);
+			$scope.addTweet(tweet);
       	});
 	}]);
